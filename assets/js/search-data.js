c65d62ccfa74d8db31faa7a39008dfb44094350e
thomasschober <117316376+thomasschober@users.noreply.github.com>
2025-04-04 00:17:07 +0000

    Deploying to gh-pages from @ thomasschober/folio3000@9e38d0965bd6159996d32a55d0250c3f6b21c0e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/folio3000/blog/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/folio3000/repositories/";
-          },
         },{id: "nav-publications",
           title: "Publications",
           description: "",
